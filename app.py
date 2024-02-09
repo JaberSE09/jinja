ffcc9bc30c7eca03eb296a3e96a7e715bba5035c
Sajid Jaber <sajidjaber@yahoo.com>
2024-02-08 19:20:02 -0600

    added some styles

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask,render_template,request
+from stories import story
+from flask_debugtoolbar import DebugToolbarExtension
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+Debug = DebugToolbarExtension(app)
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', prompts=story.prompts)
+                           
+@app.route('/story')
+def storyRoute():
+    return render_template('story.html', text=story.generate(request.args))
